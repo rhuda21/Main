6b7924d39b2117cf4c57051538c2193523613569
R H <165597973+rhuda21@users.noreply.github.com>
2025-07-27 13:50:54 +0100

    Update WebhookLibrary.lua

--- Library/WebhookLibrary.lua
+++ Library/WebhookLibrary.lua
@@ -1,5 +1,6 @@
+local WebhookLib = {}
 local http = game:GetService("HttpService")
-local function SendMessageEMBED(url, embed, mention)
+local function WebhookLib.SendMessageEMBED(url, embed, mention)
     local payload = {
         content = mention and ("<@" .. mention .. ">") or "",
         embeds = {{
@@ -27,4 +28,4 @@ local function SendMessageEMBED(url, embed, mention)
         warn("Failed to send webhook:", response)
     end
 end
-return SendMessageEMBED
+return WebhookLib
