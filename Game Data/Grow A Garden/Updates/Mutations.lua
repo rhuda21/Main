b7b75fc9414d7a0d30fa42a08ccb9d82bb1b5229
R H <165597973+rhuda21@users.noreply.github.com>
2025-07-31 06:02:16 +0100

    Update Mutations.lua

--- Game Data/Grow A Garden/Updates/Mutations.lua
+++ Game Data/Grow A Garden/Updates/Mutations.lua
@@ -17,25 +17,25 @@ for _, name in ipairs(mutationNames) do
         }
     end
 end
-local clipboardText = "return {\n"
+local mutations = "return {\n"
 for name, data in pairs(mutationsData) do
-    clipboardText = clipboardText .. string.format('    ["%s"] = {\n', name)
-    clipboardText = clipboardText .. string.format('        ["Name"] = "%s",\n', data.Name)
-    clipboardText = clipboardText .. string.format('        ["Id"] = "%s",\n', data.Id)
+    mutations = mutations .. string.format('    ["%s"] = {\n', name)
+    mutations = mutations .. string.format('        ["Name"] = "%s",\n', data.Name)
+    mutations = mutations .. string.format('        ["Id"] = "%s",\n', data.Id)
     if data.Color then
         local r, g, b = math.floor(data.Color.R * 255), math.floor(data.Color.G * 255), math.floor(data.Color.B * 255)
-        clipboardText = clipboardText .. string.format('        ["Color"] = Color3.fromRGB(%d, %d, %d),\n', r, g, b)
+        mutations = mutations .. string.format('        ["Color"] = Color3.fromRGB(%d, %d, %d),\n', r, g, b)
     else
-        clipboardText = clipboardText .. '        ["Color"] = nil,\n'
+        mutations = mutations .. '        ["Color"] = nil,\n'
     end
-    clipboardText = clipboardText .. string.format('        ["ValueMulti"] = %d,\n', data.ValueMulti)
+    mutations = mutations .. string.format('        ["ValueMulti"] = %d,\n', data.ValueMulti)
     if data.TimeData then
-        clipboardText = clipboardText .. '        ["TimeData"] = {\n'
-        clipboardText = clipboardText .. string.format('            ["TimeToChange"] = %d,\n', data.TimeData.TimeToChange)
-        clipboardText = clipboardText .. string.format('            ["AddMutationName"] = "%s"\n', data.TimeData.AddMutationName)
-        clipboardText = clipboardText .. '        },\n'
+        mutations = mutations .. '        ["TimeData"] = {\n'
+        mutations = mutations .. string.format('            ["TimeToChange"] = %d,\n', data.TimeData.TimeToChange)
+        mutations = mutations .. string.format('            ["AddMutationName"] = "%s"\n', data.TimeData.AddMutationName)
+        mutations = mutations .. '        },\n'
     end
-    clipboardText = clipboardText .. '    },\n'
+    mutations = mutations .. '    },\n'
 end
-clipboardText = clipboardText .. "}"
-return clipboardText
+mutations = mutations .. "}"
+return mutations
