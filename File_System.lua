35f28cc5528e81564ba31c468460bb0d422f40db
R H <165597973+rhuda21@users.noreply.github.com>
2025-08-27 18:05:16 +0100

    Update File_System.lua

--- File_System.lua
+++ File_System.lua
@@ -45,7 +45,7 @@ function FileLib:ReadFile(path)
 end
 
 function FileLib:WriteFile(path, data)
-    local success, result = pcall(_writefile, path, data)
+    local writeSuccess = FileLib:WriteFile(fileName, json) 
     if success then
         return result
     end
