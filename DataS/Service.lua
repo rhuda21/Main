8a953f1dd874e09df52ab33487d0adf6462767c2
R H <165597973+rhuda21@users.noreply.github.com>
2025-09-11 07:25:39 +0100

    Create Service.lua

---
+++ DataS/Service.lua
@@ -0,0 +1,12 @@
+local DataService = {}
+
+local Script = ""
+local OverideID = nil
+local GameId = OverideID or game.PlaceId
+local endpoint = "https://ub-services.rhuda21.workers.dev/loadData"
+local DataE = endpoint .. "?script=Mutations&gameId=" .. tostring(16732694052)
+
+function InitalizeData(Data)
+  
+
+return DataService
