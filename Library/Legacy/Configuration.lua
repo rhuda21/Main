6bcdd40dc7b95760782d04ba6a471f313bd59e6c
R H <165597973+rhuda21@users.noreply.github.com>
2025-09-29 22:05:56 +0100

    Create Configuration.lua

---
+++ Library/Legacy/Configuration.lua
@@ -0,0 +1,7 @@
+local UBHubOT = {}
+UBHubOT.DC = "https://discord.gg/pn8xyhuSeV"
+UBHubOT.Endpoint = "https://ubhub.pages.dev/"
+UBHubOT.Lib = "https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/V2.lua"
+UBHubOT.SaveManager = "https://gitlab.com/r_soft/main/-/raw/main/Library/SaveManagerNew.lua"
+UBHubOT.InterfaceManager = "https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/V2.lua"
+return UBHubOT
