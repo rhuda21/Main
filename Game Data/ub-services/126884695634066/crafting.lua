70afe969279521fa17a7d1494651d86dae5c5564
R H <165597973+rhuda21@users.noreply.github.com>
2025-10-25 21:21:50 +0100

    d

--- Game Data/ub-services/126884695634066/crafting.lua
+++
@@ -1,8 +0,0 @@
-local recipes = {}
-for recipeName, recipeData in pairs(require(game:GetService("ReplicatedStorage").Data.CraftingData.CraftingRecipeRegistry).ItemRecipes) do
-    local inputs = {}
-    for i, v in pairs(recipeData.Inputs) do
-        inputs[i] = {ItemType = v.ItemType,ItemData = {AcceptAllTypes = v.ItemData.AcceptAllTypes or false,ItemName = v.ItemData.ItemName}}
-    end
-    recipes[recipeName] = {Inputs = inputs, MachineTypes = recipeData.MachineTypes, Cost = recipeData.Cost}
-end return game:GetService("HttpService"):JSONEncode(recipes)
