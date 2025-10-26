caccee1c93b81a06d58a20293d90620ff65e9b12
R H <165597973+rhuda21@users.noreply.github.com>
2025-10-26 18:10:36 +0000

    Delete Library/Legacy/Configuration.lua

--- Library/Legacy/Configuration.lua
+++
@@ -1,7 +0,0 @@
-local UBHubOT = {}
-UBHubOT.DC = "https://discord.gg/pn8xyhuSeV"
-UBHubOT.Endpoint = "https://ubhub.pages.dev/"
-UBHubOT.Lib = "https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/V2.lua"
-UBHubOT.SaveManager = "https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/Legacy/Saver.lua"
-UBHubOT.InterfaceManager = "https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/V2.lua"
-return UBHubOT
