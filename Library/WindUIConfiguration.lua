9ad055867508ea0e604dd48243c6ad4af31e6abb
R H <165597973+rhuda21@users.noreply.github.com>
2026-01-23 18:22:13 +0000

    Update WindUIConfiguration.lua

--- Library/WindUIConfiguration.lua
+++ Library/WindUIConfiguration.lua
@@ -1,7 +1,7 @@
 local Saver = {}
 local savedData = {}
 local Path
-local function GetSavedVal(name,defaultVal)
+local function GetSavedVal(name, defaultVal)
     local sv = savedData[name]
     if sv == nil then return defaultVal end
     if type(defaultVal) == "table" and type(sv) ~= "table" then
@@ -84,6 +84,18 @@ Saver.PatchElement = function(elementType, UI, originalMethod)
                 Saver.Save()
                 if originalCallback then originalCallback(selected) end
             end
+        elseif elementType == "Slider" then
+            local defaultValue = config.Value and config.Value.Default or 0
+            local savedValue = GetSavedVal(elementName, defaultValue)
+            if config.Value then
+                config.Value.Default = savedValue
+            end
+            local originalCallback = config.Callback
+            config.Callback = function(value)
+                savedData[elementName] = value
+                Saver.Save()
+                if originalCallback then originalCallback(value) end
+            end
         end
         return original(self, config)
     end
@@ -91,5 +103,6 @@ end
 Saver.PatchAll = function(UI)
     Saver.PatchElement("Toggle", UI)
     Saver.PatchElement("Dropdown", UI)
+    Saver.PatchElement("Slider", UI)
 end
 return Saver
