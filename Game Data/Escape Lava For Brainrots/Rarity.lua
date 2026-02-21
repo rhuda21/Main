2f42836a3ecb43c0d8f810d58f6b21b69fb587dc
R H <165597973+rhuda21@users.noreply.github.com>
2026-02-21 12:09:56 +0000

    Create Rarity.lua

---
+++ Game Data/Escape Lava For Brainrots/Rarity.lua
@@ -0,0 +1,48 @@
+local HttpService = game:GetService("HttpService")
+local e = require(game:GetService("ReplicatedStorage").Registries.RarityRegistry)
+local function prettyPrintJson(jsonString)
+    local result = ""
+    local indentLevel = 0
+    local inString = false  
+    for i = 1, #jsonString do
+        local char = jsonString:sub(i,i)
+        if char == '"' and jsonString:sub(i-1,i-1) ~= "\\" then
+            inString = not inString
+        end
+        if not inString then
+            if char == "{" or char == "[" then
+                result = result .. char .. "\n" .. string.rep("  ", indentLevel + 1)
+                indentLevel = indentLevel + 1
+            elseif char == "}" or char == "]" then
+                indentLevel = indentLevel - 1
+                result = result .. "\n" .. string.rep("  ", indentLevel) .. char
+            elseif char == "," then
+                result = result .. char .. "\n" .. string.rep("  ", indentLevel)
+            elseif char == ":" then
+                result = result .. char .. " "
+            else
+                result = result .. char
+            end
+        else
+            result = result .. char
+        end
+    end
+    return result
+end
+local function convertToJsonFriendly(tbl)
+    local newTbl = {}
+    for key, value in pairs(tbl) do
+        if type(value) == "table" then
+            newTbl[key] = convertToJsonFriendly(value)
+        elseif typeof(value) == "Color3" then
+            newTbl[key] = string.format("Color3(%f, %f, %f)", value.R, value.G, value.B)
+        else
+            newTbl[key] = value
+        end
+    end
+    return newTbl
+end
+local friendlyTable = convertToJsonFriendly(e) 
+local jsonString = HttpService:JSONEncode(friendlyTable)
+local prettyJson = prettyPrintJson(jsonString) 
+return prettyJson
